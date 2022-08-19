2ebc4bc4d22fed91210fa73e5c143ee7e72f7fe4
Prithwis Das <prithwisdas2017@gmail.com>
2022-08-19 03:18:07 +0000

    update

--- src/datastr/DataStr.py
+++ src/datastr/DataStr.py
@@ -4,46 +4,45 @@ def display_stack(l:list, top):
     if top == l[0]:
         print("+--------------+")
         for elements in l:
-            return elements
-            print("----------------")
+            print(elements)
+            print("----------------")   
     
     elif top == l[-1]:
         print("+--------------+")
-        for elements in l.reverse():
-            return elements
+        for elements in list(reversed(l)):
+            print(elements)
             print("----------------")
+
     else:
-        return "Error"
+        print("Error")
         sys.exit(1)
+
 
 def push(l:list, top, element):
     if top == l[0]:
         l.insert(0, element)
-        return display_stack(l, top)
-        sys.exit(0)
 
     elif top == l[-1]:
         l.append(element)
-        return display_stack(l, top)
-        sys.exit(0)
-
     else:
-        return "Error"
+        print("Error")
         sys.exit(1)
+
 
 def pop(l:list, top):
     if top == l[0]:
         l.pop(0)
         return display_stack(l, top)
-        sys.exit(0)
 
     elif top == l[-1]:
         l.pop(-1)
         return display_stack(l, top)
-        sys.exit(0)
 
     else:
         return "Error"
         sys.exit(1)
 
-    
+
+le = [1, 2, 3, 4, 5, 6, 7]
+
+display_stack(push(le, 1, 8), 8)
